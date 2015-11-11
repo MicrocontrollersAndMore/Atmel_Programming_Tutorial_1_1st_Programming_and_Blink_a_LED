64582b10a47f7768ff35b2f34c91d2f5106c1135
MicrocontrollersAndMore <cdahms123@gmail.com>
2015-11-11 07:32:08 -0500

    1

--- MyAtmelBlink.c
+++ MyAtmelBlink.c
@@ -1,9 +1,6 @@
-/*
- * GccApplication3.c
- *
- * Created: 10/9/2015 6:41:00 PM
- * Author : cdahms
- */ 
+// MyAtmelBlink.c
+
+// put LED on PC5
 
 #ifndef F_CPU					// if F_CPU was not defined in Project -> Properties
 #define F_CPU 1000000UL			// define it now as 1 GHz unsigned long
@@ -15,14 +12,12 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int main(void) {
 	
-	DDRD = 0xFF;			// set Port D to output
+	DDRC |= (1 << PC5);			// set Port C pin PC5 for output
 	
-	while (1) {
-		PORTD = 0x01;			// set Port D bit 1 high
-		_delay_ms(500);			// delay 1/2 second
-		PORTD = 0x00;			// set all Port D bits low
+	while (1) {					// begin infinite loop
+		PORTC ^= (1 << PC5);	// flip state of LED on PC5
 		_delay_ms(500);			// delay 1/2 second
 	}
+	return(0);					// should never get here, this is to prevent a compiler warning
 }
-
 
