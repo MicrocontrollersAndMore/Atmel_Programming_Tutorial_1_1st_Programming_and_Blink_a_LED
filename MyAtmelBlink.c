8f87d3249ada34aa270f37c2c0e69b82fadb8711
Arda Aydın <melezorus34@gmail.com>
2020-10-02 14:21:33 +0300

    Update PIN references for Atmel Studio 7

--- MyAtmelBlink.c
+++ MyAtmelBlink.c
@@ -12,10 +12,10 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int main(void) {
 	
-	DDRC |= (1 << PC5);			// set Port C pin PC5 for output
+	DDRC |= (1 << PINC5);			// set Port C pin PC5 for output
 	
 	while (1) {					// begin infinite loop
-		PORTC ^= (1 << PC5);	// flip state of LED on PC5
+		PORTC ^= (1 << PINC5);	// flip state of LED on PC5
 		_delay_ms(500);			// delay 1/2 second
 	}
 	return(0);					// should never get here, this is to prevent a compiler warning
